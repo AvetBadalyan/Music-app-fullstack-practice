27a467aad042c87d1f2993ec51e0283617f68966
AvetBadalyan <avetbadalyan@gmail.com>
2025-01-15 20:33:43 +0400

    add .env and update data source

--- data-source.ts
+++ data-source.ts
@@ -4,14 +4,17 @@ import { Artist } from './src/entities/Artist';
 import { Album } from './src/entities/Album';
 import { Song } from './src/entities/Song';
 import { Genre } from './src/entities/Genre';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'music_app',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [Artist, Album, Song, Genre, 'src/entities/*.ts'],
